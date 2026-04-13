fd5e2ef7eab765017b4e893be9d49d63b199aec9
Neomaster X <neomasterx@outlook.com>
2026-04-13 11:40:49 +0300

    RingBuffer<TItem> ctor

--- Neomaster.RingBuffer/RingBuffer.cs
+++ Neomaster.RingBuffer/RingBuffer.cs
@@ -1,5 +1,22 @@
+using System;
+
 namespace Neomaster.RingBuffer;
 
-public class RingBuffer
+public class RingBuffer<TItem>
 {
+  private readonly TItem[] _buffer;
+  private int _head;
+
+  public RingBuffer(int capacity)
+  {
+    if (capacity <= 0)
+    {
+      throw new ArgumentOutOfRangeException(nameof(capacity), "Capacity must be greater than zero.");
+    }
+
+    _buffer = new TItem[capacity];
+    Capacity = capacity;
+  }
+
+  public int Capacity { get; }
 }
