755f87a56f2ec50899f0c19bf3f32bf54d6f04fa
Neomaster X <neomasterx@outlook.com>
2026-04-14 06:24:14 +0300

    test: ctor with valid capacity

--- Neomaster.RingBuffer.Tests/RingBufferTests.cs
+++ Neomaster.RingBuffer.Tests/RingBufferTests.cs
@@ -11,4 +11,18 @@ public class RingBufferTests
     Assert.Equal("capacity", ex.ParamName);
     Assert.Equal("Capacity must be greater than zero. (Parameter 'capacity')", ex.Message);
   }
+
+  [Fact]
+  public void Create_ValidCapacity()
+  {
+    const int capacity = 10;
+    var defaultBuffer = new int[capacity];
+
+    var buffer = new RingBuffer<int>(capacity);
+
+    Assert.Equal(capacity, buffer.Capacity);
+    Assert.Equal(defaultBuffer, buffer.Buffer.ToArray());
+    Assert.Equal([], buffer.Left.ToArray());
+    Assert.Equal(defaultBuffer, buffer.Right.ToArray());
+  }
 }
