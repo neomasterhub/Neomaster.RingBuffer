2d594cf323caaccdf4ceb7bdb3713bf1d934c333
Neomaster X <neomasterx@outlook.com>
2026-04-14 06:13:52 +0300

    test: ctor with invalid capacity

--- Neomaster.RingBuffer.Tests/RingBufferTests.cs
+++ Neomaster.RingBuffer.Tests/RingBufferTests.cs
@@ -2,4 +2,13 @@ namespace Neomaster.RingBuffer.Tests;
 
 public class RingBufferTests
 {
+  [Theory]
+  [InlineData(0)]
+  [InlineData(-1)]
+  public void Create_InvalidCapacity(int capacity)
+  {
+    var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new RingBuffer<int>(capacity));
+    Assert.Equal("capacity", ex.ParamName);
+    Assert.Equal("Capacity must be greater than zero. (Parameter 'capacity')", ex.Message);
+  }
 }
